e930631643e9913340873340b8674a405e9a7704
helixjolt <siddharth.byale17@siesgst.ac.in>
2020-04-13 11:16:46 +0530

    Splash Screen Updated

--- app/src/main/java/com/example/android/bbtracker/SplashScreen.java
+++ app/src/main/java/com/example/android/bbtracker/SplashScreen.java
@@ -2,28 +2,35 @@ package com.example.android.bbtracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 
-import gr.net.maroulis.library.EasySplashScreen;
 
 public class SplashScreen extends AppCompatActivity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 800;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EasySplashScreen config = new EasySplashScreen(SplashScreen.this)
-                .withFullScreen()
-                .withTargetActivity(MainActivity.class)
-                .withSplashTimeOut(1000)
-                .withBackgroundColor(Color.parseColor("#ff7275"))
-                .withAfterLogoText("App Name")
-                .withLogo(R.mipmap.ic_launcher);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(SplashScreen.this, MainActivity.class);
+                mainIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                SplashScreen.this.startActivity(mainIntent);
+                SplashScreen.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
 
-        config.getAfterLogoTextView().setTextColor(Color.WHITE);
+    @Override
+    public void onBackPressed() {
 
-        View easySplashScreen = config.create();
-        setContentView(easySplashScreen);
     }
 }
